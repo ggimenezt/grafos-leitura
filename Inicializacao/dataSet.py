3785ca224a8cdae162c19908c64dd01695b4d4ef
Gustavo Gimenez <ggimenezt@gmail.com>
2023-03-29 00:28:14 -0300

    Excluindo o que não será usado

--- Inicializacao/dataSet.py
+++ Inicializacao/dataSet.py
@@ -4,36 +4,12 @@ INSTITUTO DE MATEMÁTICA E COMPUTAÇÃO
 SIN110 - ALGORITMOS E GRAFOS
 Prof. Rafael Frinhani
 
-dataSet - Funções de leitura de um dataset, criação das estruturas de representação de grafos e salvamento de resultados em arquivo.
+Gustavo Gimenez Teixeira - 2021006467
 
-05/09/2022
+Grafos - Programa com funções básicas para práticas de algoritmos em grafos.
+Classe principal - desenvolvido em Python 3.10.6
+
+30/03/2023
 ===================================================="""
 
 import numpy as np
-
-'''Cria Matriz de Adjacência: Função para leitura de um dataset em forma de matriz de adjacências.
-Entrada: instancia (nome do arquivo .txt do dataset em forma de matriz de adjacência
-Saída: matriz de adjacência (tipo NumPy.ndarray)'''
-
-
-def criaMatrizAdjacencias(instancia):
-    print('NOME DA INSTÂNCIA:', instancia, '\n')
-    caminho = 'D:/01-Academicos/01-UNIFEI/Disciplinas/SIN110 - Algoritmos e Grafos/Aulas/Codigos/grafo/Instancias/' + instancia + '.txt'
-    with open(caminho, 'rb') as f:
-        data = np.genfromtxt(f, dtype="int64")  # OBS. Lê arquivo .txt de uma Instancia no formato Matriz de Adjacência
-    return data
-
-
-"""Salva Resultado: Função para salvar em arquivo .txt o resultado da execução do programa.
-Entrada: resultado (tipo lista)
-Saída: arquivo .txt no diretório especificado"""
-
-
-def salvaResultado(resultado):
-    stringRes = ''
-    for res in resultado:
-        stringRes += str(res) + ' '
-    arquivo = open('D:/01-Academicos/01-UNIFEI/Disciplinas/SIN110 - Algoritmos e '
-                   'Grafos/Aulas/Codigos/grafo/Resultados/resultado.txt', 'a+')
-    arquivo.writelines(stringRes + '\n')
-    arquivo.close()
--- main.py
+++ main.py
@@ -4,31 +4,24 @@ INSTITUTO DE MATEMÁTICA E COMPUTAÇÃO
 SIN110 - ALGORITMOS E GRAFOS
 Prof. Rafael Frinhani
 
+Gustavo Gimenez Teixeira - 2021006467
+
 Grafos - Programa com funções básicas para práticas de algoritmos em grafos.
 Classe principal - desenvolvido em Python 3.10.6
 
-05/09/2022
+30/03/2023
 ===================================================="""
 
+import sys
 from igraph import *
-from Inicializacao import (dataSet as ds, grafo as g, visualizacao as vis)
+from Inicializacao import (dataSet as ds)
 from Metodos import (caracteristicas as car)
 
 '''Core do programa'''
 
 
 def main(instancia):
-    matriz = ds.criaMatrizAdjacencias(instancia)
-    print(matriz, '\n')  # '\n' para inserir linha em branco ao final do comando
-
-    G = g.criaGrafo(matriz)
-    print(G, '\n')  # Mostra as características do grafo.
-
-    vis.visualizarGrafo(False, G)  # True para visualização do grafo ou False.
-    funcao1 = car.verificaAdjacencia(matriz, 0, 1)
-
-    resultado = [instancia, funcao1]  # Lista de tipo misto com valores dos resultados
-    ds.salvaResultado(resultado)  # Salva resultado em arquivo
+    x = 0
 
 
 """Chamada a função main()
